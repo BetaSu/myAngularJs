e099048adc9de302bc087640de9f2a71e644b238
betaSu <313439271@qq.com>
2016-10-01 11:08:20 +0800

    v1.0.2

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+/**
+ * 作用域及脏值查询部分
+ *
+ */
+
 var Scope=function  () {
     //用于存放监听器的监听器池
     this.$$watchers=[];
+    //需要延迟加载的队列
+    this.$$asyncQueue=[];
 }
 
 //注册监听器
@@ -32,9 +39,47 @@ Scope.prototype.$digest=function () {
     //脏值查询最大次数
     var time=10;
     do {
+        while (this.$$asyncQueue.length) {
+            var task=this.$$asyncQueue.shift();
+            this.$eval(task.expression);
+        }
         dirty=this.$$digestOnce();
         if (dirty && !(time--)) {
             throw '达到脏值检查上限'+time+'次';
         }
     } while (dirty);
 }
+
+/**
+ * 在作用域中执行外部函数
+ * @param fn 要执行的函数
+ * @param locals 传递的参数
+ * @returns fn的返回值
+ */
+Scope.prototype.$eval=function  (fn,locals) {
+    return fn(this,locals);
+}
+
+/**
+ * 在作用域中执行外部函数并触发脏值查询
+ * @param fn 要执行的函数
+ */
+Scope.prototype.$apply=function  (fn) {
+    try {
+        this.$eval(fn);
+    } finally {
+        this.$digest();
+    }
+}
+
+/**
+ * 将需要延迟加载的函数推入队列
+ * @param fn
+ */
+Scope.prototype.$evalAsync=function  (fn) {
+    this.$$asyncQueue.push({scope:this,expression:fn});
+}
+
+
+
+
